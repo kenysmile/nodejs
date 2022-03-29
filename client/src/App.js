7786b65f3bc8fd531fb0c64ecc91ef55a458efd2
letuss004 <letuss004@gmail.com>
2022-03-29 09:12:32 +0700

    em tu fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,43 +1,42 @@
-import { Fragment } from 'react'
+import {Fragment} from 'react'
 import './App.css'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Login from '../src/components/auth/Login';
 import Register from '../src/components/auth/Register';
 import Alert from './components/layout/Alert';
-import React, { useEffect } from "react";
+import React, {useEffect} from "react";
 // Redux
-import { Provider } from 'react-redux'
+import {Provider} from 'react-redux'
 import store from './store';
 import setAuthToken from './utils/setAuthToken';
 import './App.css'
 // import { loadUser } from './actions/auth';
 
 if (localStorage.token) {
-  setAuthToken(localStorage.token)
+    setAuthToken(localStorage.token)
 }
 
-const App = () => 
-  // useEffect(() => {
-  //   store.dispatch(loadUser())
-  // }, [])
+const App = () =>
+    // useEffect(() => {
+    //   store.dispatch(loadUser())
+    // }, [])
 
-  <Provider store={store}>
-    <Router>
-    <Fragment>
-      <Navbar />
-      <Route exact path='/' component={Landing} />
-      <section className='container'>
-        <Alert />
-        <Switch>
-          <Route exact path="/register" component={Register} />
-          <Route exact path="/login" component={Login} />
-
-        </Switch>
-      </section>
-      </Fragment>
-    </Router>
-  </Provider>
+    <Provider store={store}>
+        <Router>
+            <Fragment>
+                <Navbar/>
+                <Route exact path='/' component={Landing}/>
+                <section className='container'>
+                    <Alert/>
+                    <Switch>
+                        <Route exact path="/register" component={Register}/>
+                        <Route exact path="/login" component={Login}/>
+                    </Switch>
+                </section>
+            </Fragment>
+        </Router>
+    </Provider>
 
 export default App
--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,21 +1,18 @@
 import React, { Fragment, useState } from 'react';
 import { Link } from 'react-router-dom';
+
+
+
 const Login = () => {
     const [formData, setFormData] = useState({
         email: '',
         password: '',
     })
-
-
     const { email, password } = formData
-
     const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value })
-
     const onSubmit = async e => {
         e.preventDefault();
         console.log('Success')
-        
-
     }
 
     return <Fragment>
